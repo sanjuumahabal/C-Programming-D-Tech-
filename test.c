1dd519acc35cb687a27012602cac5efef4cdd8f8
sanjuumahabal <168001917+sanjuumahabal@users.noreply.github.com>
2025-06-26 18:02:16 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,24 @@
+int main(){
+	int num_1=57;		//declaration statement
+
+	int num2;			//decleration statement
+	num2=758475;
+	
+	char ch1='b';
+	
+	char ch2;
+	ch2 = 'k';
+	
+	float num4=78;
+	
+	int num5 = 56.45;
+	
+	double d2 = 746.5847;
+	
+//	'*', '@','#'
+	
+//	Syntax:
+//		data_type variable_name = value;
+//	num3
+	return 0;
+}
