628fa7b4f4723068a719d463ed75c66191443192
sanjuumahabal <168001917+sanjuumahabal@users.noreply.github.com>
2025-07-01 19:06:30 +0530

    Add files via upload

---
+++ decision_making_if_else.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+//	float age;
+//	
+//	printf("Enter Your Age: ");
+//	scanf("%f",&age);
+	
+	//age = 15
+	
+//	if statement syntax
+//	if(condition){
+//		
+//	}
+	
+	//12
+	
+//	if(age>=18){
+//		printf("You are Eligible to Vote!!\n");
+//	}
+//	else{
+//		printf("You are not Eligible to Vote!!\n");
+//	}
+	
+	
+// WAP to check if a number is positive or negative.
+	
+//	float num;
+//	
+//	printf("Enter a Number: ");
+//	scanf("%f",&num);
+//	
+//	if(num<0){
+//		printf("Negative Number!!");
+//	}
+//	else{
+//		printf("Positive Number !!");
+//	}
+
+//	WAP to check for a even or odd number.
+
+	float num;
+	printf("Enter a Number: ");
+	scanf("%f",&num);	
+	
+	if(num%2==0){
+		printf("%f is Even.\n",num);
+	}
+	else{
+		printf("%f is Odd.\n",num);
+	}
+	
+	
+	return 0;
+}
