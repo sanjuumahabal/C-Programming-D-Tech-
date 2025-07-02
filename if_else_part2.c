0901c2f27dcc1699efc88883763be341e2598e74
sanjuumahabal <168001917+sanjuumahabal@users.noreply.github.com>
2025-07-02 19:06:07 +0530

    Add files via upload

---
+++ if_else_part2.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int main(){
+//	int a;
+//	int b;
+//	int c;
+//	
+//	printf("Enter first number :- ");
+//	scanf("%d",&a);
+//	printf("Enter second number :- ");
+//	scanf("%d",&b);
+//	printf("Enter third number :- ");
+//	scanf("%d",&c);
+	
+	// program for checking positve, negative or zero
+	
+//	if(a>1){
+//		printf("Positive Number !!");
+//	}
+//	else if(a==0){
+//		printf("Zero !!");
+//	}
+//	else{
+//		printf("Negative Number !!");
+//	}
+
+
+
+
+// nested if else
+// WAP to check for leap year.
+
+//	int year;
+//	printf("Enter year: ");
+//	scanf("%d",&year);
+//	
+//	//1904
+//	if(year%100==0){		//condition for century year
+//		if(year%400==0){
+//			printf("Leap Year !!");
+//		}
+//		else{
+//			printf("Not a Leap Year !");
+//		}
+//	}
+//	else{
+//		if(year%4==0){
+//			printf("Leap Year !!");
+//		}
+//		else{
+//			printf("Not a Leap Year !");
+//		}
+//	}
+
+
+	int num1 = 45, num2=56, num3=89;
+	
+	
+
+	return 0;
+}
