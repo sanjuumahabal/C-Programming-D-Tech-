1c841915e3ce73f09bef76087a9fc67bead5c9dd
sanjuumahabal <168001917+sanjuumahabal@users.noreply.github.com>
2025-07-04 19:10:53 +0530

    Add files via upload
    while loop problems

---
+++ while_loop.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+//Iterative Control Statements
+/*
+	1. while loop
+	2. do-while loop
+	3. for loop
+*/
+
+int main(){
+	// while loop
+	
+//	if(1<2){
+//		printf("Hello one is less than two\n");
+//	}
+	
+//	while(1<2){			//infitinte loop
+//		printf("Hello one is less than two\n");
+//		printf();
+//		printf();
+//		printf();
+//	}
+	
+//	while(2<3){					//infinite loop with break statement
+//		printf("hello \n");
+////		break;
+////		printf("This statement is after break\n");	//unreachable code due to break	
+//	}
+	
+	
+//	int i=5;
+//	// print hello 5 times
+//	while(i>1){
+//		printf("Hello\n");
+////		i=i+1;
+////		i+=1;		// i = i+1;
+//		i-=1;		// i = i -1;
+////		i++;		// i=i+1;
+//	}
+
+
+	//print first 10 natural numbers
+	
+//	int i=11;
+//	
+//	while(i>0){
+//		printf("%d\n",i);
+//		i--;
+//	}
+
+
+	int month=2;
+	
+	if(month==1){
+		printf("Jan - 31 days\n")
+	}
+	else if(month==2){
+		printf("Feb - 28/29 days\n");
+	}
+	else if(month==3){
+		printf("March - 31 days\n");
+	}
+	else{
+		printf("Invalid \n");
+	}
+	
+	
+	
+	
+	return 0;
+}
+
