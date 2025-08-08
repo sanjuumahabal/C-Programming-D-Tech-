3df8c37a285c07d0cbe954c4e3a6ff4ccf342a87
sanjuumahabal <168001917+sanjuumahabal@users.noreply.github.com>
2025-08-08 19:04:20 +0530

    Add files via upload

---
+++ strings_part1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//STRINGS
+//WAP to count no. of vowels in a string.
+
+void noOfVowels(char str[]){
+	int i;
+	
+	for(){
+		
+	}
+}
+
+int main(){
+	int arr[15] = {34,56,78,97,66,43};
+	
+	char str[20] = {'d','t','e','c','h',' ','c','o','m','p','u','t','e','r','s'};
+	
+	char str1[10] = "sagar";
+	
+	
+	int i;
+	
+	printf("%s\n",str);
+	
+	printf("%s\n",str1);
+	
+	
+	//printing character array
+//	for(i=0; str[i]; i++){
+//		printf("%c",str[i]);
+//	}
+
+	
+	return 0;
+}
+
